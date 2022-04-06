f9bcfd76aa7d65f5ba4b864e0b9fc3e311452cbd
Melvin <melvingit@gmail.com>
2022-04-06 02:42:55 -0700

    added a bit to test

--- src/TreeTest.java
+++ src/TreeTest.java
@@ -1,3 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+//import;
+
+import static org.junit.Assert.assertEquals;
+
 public class TreeTest {
-    
+    @Test
+    void testpostorderTraverse(){
+        AssertEquals
+    }
 }
